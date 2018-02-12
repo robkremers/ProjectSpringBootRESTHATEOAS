5bcd572dbda23e5834b36f946800da068ee3cf42
robkremers <robkremers@hotmail.com>
2018-02-12 17:31:25 +0100

    Initial commit

---
+++ src/main/java/com/rkremers/rest/ProjectSpringBootResthateoasApplication.java
@@ -0,0 +1,12 @@
+package com.rkremers.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringBootResthateoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringBootResthateoasApplication.class, args);
+	}
+}
